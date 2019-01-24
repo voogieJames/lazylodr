c942c629af2a7097a0fc2ad829d63d04b3d5bf64
Vugar Abdulzada <vugar@origin.no>
2019-01-24 09:12:01 +0100

    Finally figured out why it didn't worked...

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,35 @@
 document.addEventListener("DOMContentLoaded", function() {
+    var gallery = document.querySelector(".gallery");
+    for (var i = 0; i < 100; i++){
+        gallery.appendChild(createLazyImg());
+    }
+    lazylodr(gallery);
+});
+
+function createLazyImg(src, alt, title) {
+    let img = document.createElement('img');
+    img.className = "lazy";
+    img.style.width = "640px";
+    img.style.height = "480px";
+    if (src != null) 
+        img.src = src
+    else 
+        img.setAttribute("data-src", "https://source.unsplash.com/random/" + Math.floor(600 + Math.random() * 100));
+    (alt != null) && (img.alt = alt)
+    (title != null) && (img.title = title)
+    return img;
+}
+
+function lazylodr(rootNode) {
     var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
 
     if ("IntersectionObserver" in window) {
 
-        var _onIntersection = function(entries, observer) {
+        var _onIntersection = function(entries) {
             entries.forEach(function(entry) {
-                if (entry.isIntersecting) {
+                if (entry.isIntersecting && entry.intersectionRatio > 0) {
                     let lazyImage = entry.target;
                     lazyImage.src = lazyImage.dataset.src;
-                    lazyImage.srcset = lazyImage.dataset.srcset;
                     lazyImage.classList.remove("lazy");
                     lazyImageObserver.unobserve(lazyImage);
                 }
@@ -16,11 +37,10 @@ document.addEventListener("DOMContentLoaded", function() {
         }
 
         var config = {
-            root: document.getElementById('root'),
+            root: rootNode,
             rootMargin: '50px 0px',
             treshold: 0.01
         };
-
 
         let lazyImageObserver = new IntersectionObserver(_onIntersection, config);
 
@@ -30,5 +50,5 @@ document.addEventListener("DOMContentLoaded", function() {
     } else {
         //  todo: add second, more compatible method here lol
     }
+}
 
-});
